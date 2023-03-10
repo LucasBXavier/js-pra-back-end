3e09b3a08085529f09ddcdd2f4e396a9409ee2d2
LucasBXavier <lucas_b.xavier@hotmail.com>
2023-03-09 21:32:09 -0300

    criando array com "new"

--- js-arrays/div-sala.js
+++ js-arrays/div-sala.js
@@ -3,5 +3,13 @@ const alunos = ['João', 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Guilherm
 const sala1 = alunos.slice(0, alunos.length / 2);
 const sala2 = alunos.slice(alunos.length / 2)
 
+const alunosReprovados = new Array( //cria uma nova array com 'new'
+    'João',
+    'Ana',
+    'Marjorie'
+)
+
+alunosReprovados.pop(3)
 console.log(sala1);
 console.log(sala2);
+console.log("os alunos reprovados foram:" ,alunosReprovados)
